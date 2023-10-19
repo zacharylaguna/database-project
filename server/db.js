ad510396530a6e5045c0ac128c7e7680a4867ee8
EC2 Default User <ec2-user@ip-172-31-21-173.us-east-2.compute.internal>
2023-10-19 21:57:27 +0000

    update .gitignore and dev database

--- server/db.js
+++ server/db.js
@@ -3,7 +3,7 @@ const Pool = require("pg").Pool;
 const pool = new Pool({
     host     : 'dev-database.cfa8nnooilud.us-east-2.rds.amazonaws.com',
     user     : 'postgres',
-    password : 'Kbt2dDjlynYb1zvMHT2C',
+    password : 'devpass09222023',
     port     : '5432',
     database : "real-mgmt"
 });
